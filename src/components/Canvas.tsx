e1927154aa9bccd0ddf553e820825fd253fc27ef
DualDen <disa.shilow2@gmail.com>
2023-02-08 22:32:03 +0700

    ref

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,7 +2,7 @@ import React, {FC, useEffect, useRef} from 'react';
 import "../styles/canvas.scss";
 import {canvasSlice} from "../store/reducers/CanvasSlice";
 const Canvas:FC = () => {
-    const canvasRef = useRef();
+    const canvasRef = useRef() as React.MutableRefObject<HTMLCanvasElement>;
     const {setCanvas} = canvasSlice.actions;
     useEffect(() => {
        setCanvas('');
@@ -10,7 +10,7 @@ const Canvas:FC = () => {
 
     return (
         <div className='canvas'>
-            <canvas ref={canvasRef} width={800} height={600}></canvas>
+            <canvas ref={canvasRef} width={800} height={600}/>
         </div>
     );
 };
